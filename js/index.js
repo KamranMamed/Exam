94d6c49a9ab79d55c51aad9d3022db51009612de
Kamran <126233821+KamranMamed@users.noreply.github.com>
2023-05-21 20:57:57 +0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+    isNavbarExpanded = !isNavbarExpanded;
+    navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+
+
+// Секция 4 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var sections = document.getElementsByClassName('accordion-section');
+
+    Array.prototype.forEach.call(sections, function (section) {
+        section.addEventListener('click', function () {
+            var content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
